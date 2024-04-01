3cd34ce8b5736b7419bce526b3b54d0a7f229528
jathursika <154547845+Jathursi@users.noreply.github.com>
2024-04-01 20:17:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 function App() {
   return (
-          <Router basename="/dog">
+          <BrowserRouter basename="/dog">
             <Routes>
               <Route path='/' element={<Home/>}></Route>
             </Routes>
-          </Router>
+          </BrowserRouter>
   );
 }
 
